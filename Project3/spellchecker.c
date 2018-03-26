ca38889d64cfe5f9d8ef9c47bd248bee6dbddc59
My Dear Sir <saints.fanatic@yahoo.com>
2018-03-26 19:39:44 +0000

    middle of writing spellchecker lib:

--- Project3/spellchecker.c
+++ Project3/spellchecker.c
@@ -1,37 +1,48 @@
-
+#include "spellchecker.h"
 // /*
 //  *  Main loop
 //  */
 // boolean_t loop();
 
-int main(int argc, char *argv[])
+// int main(int argc, char *argv[])
+// {
+//     if( initialize() == NULL ){
+//         puts("Can't read dictionary file.");
+//         return 1;
+//     }
+    
+//     if(argc > 1)
+//         printf( "%s%s", argv[1], searchword( argv[1] ) ? " is correct\n" : " is incorrect\n");
+    
+//     fclose(dictionary);
+//     return 0;
+// }
+
+
+Wordslist_t spellcheck_initialize(char *filename)
 {
-    if( ! initialize() ){
-        puts("Can't read dictionary file.");
-        return 1;
+    
+    
+    Wordslist_t wordsfile;
+    // Check for filename and try to open it
+    if(filename != NULL){
+        wordsfile.dictionary = fopen(filename, "r");
     }
-    
-    if(argc > 1)
-        printf( "%s%s", argv[1], searchword( argv[1] ) ? " is correct\n" : " is incorrect\n");
-    
-    fclose(dictionary);
-    return 0;
+    if( filename == NULL || wordsfile.dictionary = NULL){
+        wordsfile.dictionary = fopen("dictionary", "r");
+    }
+    if(Wordsfile.dictionary != NULL) return Wordsfile;
+    return NULL;
 }
 
-boolean_t initialize()
+boolean_t spellcheck_searchword(Wordslist_t *wl, char *target)
 {
-    dictionary = fopen("dictionary", "r");
-    return dictionary != NULL;
-}
-
-boolean_t searchword(char *target)
-{
-    rewind(dictionary);
+    rewind(wl->dictionary);
     char buf[30];
-    while( !feof(dictionary) ){
+    while( !feof(wl->dictionary) ){
         // Trim newline
         
-        fgets(buf, 30, dictionary);
+        fgets(buf, 30, wl->dictionary);
         strcpy(buf, strtok(buf, " \n"));
         if( ! strcmp(buf, target) ) return true;
     }
--- Project3/spellchecker.h
+++ Project3/spellchecker.h
@@ -7,19 +7,31 @@
 typedef enum { false = 0, true = 1 } boolean_t;
 #endif
 
-// File hold file pointer for words list.
-FILE *dictionary;
-
 /*
- *  Initialize spellchecker by reading in dictionary.
- *  return - true if dictionary file open for reading, else false
+ *  Wrapper structure for shared file pointer. Exists only to allow calling
+ *  processes to initialize variables without this code living in any process.
  */
-boolean_t initialize();
+typedef struct {
+    // File hold file pointer for words list.
+    FILE *dictionary;
+} Wordslist_t;
+
+Wordslist_t currentData;
 
 /*
- *  Read lines of dictionary looking for one that matches argument. Return false
-    if end of file reached.
+ *  Initialize spellchecker by reading in dictionary. If valid file given in
+    argument, read file in as dictionary. Put FILE struct in wrapper Wordslist_t
+    struct if file read and return struct. Else return NULL.
+ *  param filename - String containing filename to use as dictionary
+ *  return - struct containing dictionary file if file open for reading, else NULL
+ */
+Wordslist_t spellcheck_initialize(char *filename);
+
+/*
+ *  Read lines of dictionary looking for one that matches argument. 
  *  param target - word to search for
+ *  param wl - struct containing dictionary file struct
  *  return - true if match found, else false
  */
-boolean_t searchword(char *target);
+boolean_t spellcheck_searchword(Wordslist_t wl, char *target);
+

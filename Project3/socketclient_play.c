44430433dbd1f398bf5188de82adacb07d040b8e
My Dear Sir <saints.fanatic@yahoo.com>
2018-03-26 13:39:40 +0000

    Server has global array of sockets that read correctly in functions, and dictionary file exists.

--- Project3/socketclient_play.c
+++ Project3/socketclient_play.c
@@ -5,7 +5,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <string.h>
-#include "open_listenfd.c"
+//#include "open_listenfd.c"
 
 int main(int argc, char *argv[])
 {
--- Project3/socketserver_play.c
+++ Project3/socketserver_play.c
@@ -8,6 +8,7 @@
 #include "../csapp.c"
 
 void* showfirst(void*);
+int sd[5];
 
 int main(int argc, char *argv[])
 {
@@ -18,15 +19,17 @@ int main(int argc, char *argv[])
     int i;
     for(i = 0; i < 5; i++){
         int connectionSocket = accept(listeningSocket, NULL, NULL);
-        pthread_create(workerthread, NULL, &showfirst, connectionSocket);
+        sd[0] = connectionSocket;
+        pthread_create(&workerthread, NULL, &showfirst, connectionSocket);
     }
 
     return 0;
 }
 
-void *showfirst(void *connectionSocket)
+void *showfirst(void *varg)
 {
     char buf[100];
+    int connectionSocket = sd[0];
     recv(connectionSocket, buf, 100, NULL);
     printf("Received %s\n", buf);
 }

64a0b820ea8da0a5ee1521ab1b69ae99d29b8b51
Shmuel Jacobs <saints.fanatic@yahoo.com>
2018-03-29 16:08:52 -0400

    Concurrency apparently achieved

--- Stallings-Ch5/Monitor5-16.c
+++ Stallings-Ch5/Monitor5-16.c
@@ -48,13 +48,15 @@ int main(int argc, char *argv[])
     pthread_create(&threadc, NULL, &consumer, NULL);
 
     pthread_join(threadp, NULL);
+    puts("Producter Returned and died.\n");
     pthread_join(threadc, NULL);
 
     return 0;
 }
 void append(char ch)
 {
-    if(count == LENGTH){
+    pthread_mutex_lock(&mutex);
+    while(count == LENGTH){
         puts("Producer waiting for buffer not full");
         pthread_cond_wait(&notfull, &mutex);
     }
@@ -62,13 +64,15 @@ void append(char ch)
     printf("Just put %c in the buffer.\n", ch);
     last = ++last % LENGTH;
     count++;
-    puts("Producer signals buffer not empty");
+    puts("Producer signals buffer not empty.\n");
     pthread_cond_signal(&notempty);
+    pthread_mutex_unlock(&mutex);
 }
 
 void take(char s)
 {
-    if(count == 0){ // Buffer is empty. Block until it isn't.
+    pthread_mutex_lock(&mutex);
+    while(count == 0){ // Buffer is empty. Block until it isn't.
         pthread_cond_wait(&notempty, &mutex);
         puts("Consumer got signal buffer not empty");
     }
@@ -77,7 +81,10 @@ void take(char s)
     wrapper.ch = c;
 
     first = ++first % LENGTH;
+    count--;
+    puts("Consumer signals buffer not full.\n");
     pthread_cond_signal(&notfull);
+    pthread_mutex_unlock(&mutex);
 }
 
 void *producer(void *noarg)
@@ -90,12 +97,14 @@ void *producer(void *noarg)
         if(x == 'q') break;
         append(x);
     }
+    printf("\nProducer left main loop.\nReturning.\n");
+    return;
 }
 
 void *consumer(void *noarg){
     char x;
     char i;
-    for(i = 0; i < 26; i++){
+    for(i = 0; i < 'q' - 'a'; i++){
         take(x);
         // puts("Got another letter from buffer.");
         // printf("%c\n", wrapper.ch);

75e2207e25981666ffcb05b5deda3b089a486f5c
Shmuel Jacobs <saints.fanatic@yahoo.com>
2018-03-30 17:33:57 -0400

    Project3.h has large struct with both buffers and setup, tear down, FIFO push and FIFO pull\nMain has concurency for sockets buffer

--- Project3/Project3.h
+++ Project3/Project3.h
@@ -1,3 +1,6 @@
+#include "../csapp.c"
+
+/*
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/types.h>
@@ -5,10 +8,126 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <pthread.h>
-#include "../csapp.c"
+
+#ifndef CSAPP
+#define CSAPP
+
+#endif
+*/
 
 const char *DEFAULT_DICTIONARY = "dictionary";
 const int DEFAULT_PORT = 5000;
 const int NUM_WORKERS = 5;
 const int Q_LEN = 20;
 
+#ifndef BOOLEAN_T
+#define BOOLEAN_T
+typedef enum { false = 0, true = 1 } boolean_t;
+#endif
+
+
+/*********************************
+Fixed size circular buffer type
+Dumb struct and methods. Will over-write itself.
+Requires CV, mutex and adult supervision.
+*********************************/
+typedef struct {
+    // Array of integers
+    int *sd_arr;
+    // Number of integers spaces allocated
+    char sd_length;
+    // Number of integers curently stored
+    char sd_count;
+    // Position of least recent integer insertion
+    char sd_first;
+    // Position of most recent integer insertion
+    char sd_last;
+
+    // Array of strings
+    char **msg_arr;
+    // Number of strings spaces allocated
+    char msg_length;
+    // Number of strings curently stored
+    char msg_count;
+    // Position of least recent string insertion
+    char msg_first;
+    // Position of most recent string insertion
+    char msg_last;
+} mx_circbuf_t;
+
+/*
+ *  Setup structure, zero out values and allocate space.
+ *  Input:
+    newbuf - Pointer to location of new structure.
+    len - number of items to store in buffer.
+ */
+void mx_circbuf_init(mx_circbuf_t *newbuf, int len)
+{
+    newbuf->sd_arr = (int*) malloc(len * sizeof(int));
+    newbuf->sd_length = len;
+    newbuf->sd_count = 0;
+    newbuf->sd_first = 0;
+    newbuf->sd_last = 0;
+
+
+}
+
+void mx_circbuf_acceptsd(mx_circbuf_t *buffer, int input)
+{
+    buffer->sd_arr[buffer->sd_last++] = input;
+    buffer->sd_last = buffer->sd_last % buffer->sd_length;
+    buffer->sd_count++;
+}
+
+int mx_circbuf_offersd(mx_circbuf_t *buffer)
+{
+    int output = buffer->sd_arr[buffer->sd_first++];
+    buffer->sd_first = buffer->sd_first % buffer->sd_length;
+    buffer->sd_count--;
+    return output;
+}
+
+void mx_circbuf_destructor(mx_circbuf_t *DOA)
+{
+    char i;
+    for(i = 0; i < DOA->msg_length; i++){
+        free(DOA->msg_arr[i]);
+    }
+    free(DOA->msg_arr);
+    free(DOA->sd_arr);
+    free(DOA);
+}
+
+/*
+void mx_circbuf_destructor(mx_circbufint_t *DOA)
+{
+    char i;
+
+    free(DOA->content);
+    free(DOA);
+} */
+
+/************************************************
+Function headers for Networked Spell Checker.
+************************************************/
+/*
+ * Safely insert a socket descriptor into a buffers struct
+    concurrently with other threads.
+
+ *  Procedure:
+    Lock socket_requests buffer.
+    Wait for buffer not full.
+    Put socket descriptor in next spot in buffer.
+    Signal buffer not empty.
+    Unlock buffer.
+
+ *  Inputs
+    sd - socketdescriptor for connected client.
+ */
+ void append_sockets(int sd);
+
+/*
+ *  Safely remove socket descriptor from buffers struct concurrently
+    with other processes.
+ */
+int take_socket();
--- Project3/main.c
+++ Project3/main.c
@@ -1,21 +1,49 @@
-// Struct Job_Complete: {String word, int found, worker}
-// 
-// Main Thread:
-//     Setup:
-//         Create Circular Buffer using buf from p_thread library.
-//         Create Pool of Five Threads.
-//         Create Logger Thread.
-//         Load dictionary from file.
-//         Initialize waiting_jobs queue of file descriptors of requests.
-//         Open Socket.
-//
-//     Main:
+#include "Project3.h"
+
+pthread_cond_t socket_requests_notfull, socket_requests_notempty;
+pthread_cond_t log_queue_notfull, log_queue_notempty;
+pthread_mutex_t sockets_lock, log_lock;
+
+pthread_t *workers;
+char *worker_ids;
+mx_circbuf_t buffers;
+
+int main(int argc, char *arg[])
+{
+    /**  Setup: **/
+
+    /* Create Pool of Five Threads. Declarations are global in heap
+    to minimize passing and help avoid losing in thread. */
+    workers = (pthread_t*) malloc(NUM_WORKERS * sizeof(pthread_t));
+    worker_ids = (char*) malloc(NUM_WORKERS);
+
+    //  Initialize queue of socket descriptors for requests.
+    mx_circbuf_init(&buffers, Q_LEN);
+
+    // Create Condition variables and lock for each queue.
+    pthread_cond_init(&socket_requests_notfull, NULL);
+    pthread_cond_init(&socket_requests_notempty, NULL);
+    pthread_cond_init(&log_queue_notfull, NULL);
+    pthread_cond_init(&log_queue_notempty, NULL);
+    pthread_mutex_init(&sockets_lock, NULL);
+    pthread_mutex_init(&log_lock, NULL);
+
+    //  Open Listening Socket.
+    int listeningSocket = open_listenfd(5000);
+
+
+
+
+    // Sit in loop producing connected sockets to clients.
+
 //         Busy wait on socket.
 //         Block on fd queue full.
 //         When job comes in, put fd in queue.
 //         Signal job in fd queue.
 //
 //
+}
+
 // Worker Threads:
 //     Block on fd queue empty.
 //     When signaled, take a client from queue and service.
@@ -32,3 +60,38 @@
 //     When jobs complete queue not empty:
 //         Get job from queue.
 //         Add to log file.
+
+void append_sockets(int sd){
+    // Lock socket_requests buffer
+    pthread_mutex_lock(&sockets_lock);
+    // As long as buffer is full.
+    while(buffers.sd_count == buffers.sd_length){
+        // Block until buffer has room and then try again.
+        pthread_cond_wait(&socket_requests_notfull, &sockets_lock);
+    }
+    // Put socket in buffer
+    mx_circbuf_acceptsd(&buffers, sd);
+
+    // Signal socket_requests not empty
+    pthread_cond_signal(&socket_requests_notempty);
+    // Unlock socket_requests
+    pthread_mutex_unlock(&sockets_lock);
+}
+
+int take_socket()
+{
+    pthread_mutex_lock(&sockets_lock);
+    // As long as buffer is empty
+    while(buffers.sd_count == 0){
+        // Block until it's not empty and then try again.
+        pthread_cond_wait(&socket_requests_notempty, &sockets_lock);
+    }
+    // Get next socket
+    int sd = mx_circbuf_offersd(&buffers);
+    // Signal socket_requests not full
+    pthread_cond_signal(&socket_requests_notfull);
+    // Unlock socket_requests
+    pthread_mutex_unlock(&sockets_lock);
+
+    return sd;
+}

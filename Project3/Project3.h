7c65d6d3f3cbe156fc761326f044fe0598cf2ac0
Shmuel Jacobs <saints.fanatic@yahoo.com>
2018-04-10 22:40:31 -0400

    Don't know why old header was here, but I put in the real header.

--- Project3/Project3.h
+++ Project3/Project3.h
@@ -17,7 +17,7 @@
 #endif
 */
 
-const char *DEFAULT_DICTIONARY = "dictionary";
+char *DEFAULT_DICTIONARY = "dictionary";
 const char LONGEST_WORD = 30;
 const int DEFAULT_PORT = 5000;
 const int NUM_WORKERS = 5;
@@ -71,10 +71,19 @@ void mx_circbuf_init(mx_circbuf_t *newbuf, int len)
     newbuf->sd_count = 0;
     newbuf->sd_first = 0;
     newbuf->sd_last = 0;
-
-
+    
+    char log_q = 100;
+    newbuf->msg_arr = (char**) malloc(len * sizeof(char*));
+    newbuf->msg_length = log_q;
+    newbuf->msg_count = 0;
+    newbuf->msg_first = 0;
+    newbuf->msg_last = 0;
+    
 }
 
+/*
+ *  Put new socket into sockets buffer. Increment insertion position and count.
+ */
 void mx_circbuf_acceptsd(mx_circbuf_t *buffer, int input)
 {
     buffer->sd_arr[buffer->sd_last++] = input;
@@ -82,11 +91,41 @@ void mx_circbuf_acceptsd(mx_circbuf_t *buffer, int input)
     buffer->sd_count++;
 }
 
+/*
+ *  Get socket from sockets buffer. Increment removal position and decrement count.
+ *  Return: Socket descriptor removed from buffer.
+ 
+ *  Data isn't removed in the sense that it's overwritten. Circular buffer records
+    position to get next data from, and that position gets incremented.
+ */
 int mx_circbuf_offersd(mx_circbuf_t *buffer)
 {
     int output = buffer->sd_arr[buffer->sd_first++];
     buffer->sd_first = buffer->sd_first % buffer->sd_length;
     buffer->sd_count--;
+    return output;
+}
+
+/*
+ *  Put new log message into msg buffer. Increment insertion position and count.
+ */
+void mx_circbuf_acceptmsg(mx_circbuf_t *buffer, char *input){
+    buffer->msg_arr[buffer->msg_last++] = input;
+    buffer->msg_last = buffer->msg_last % buffer->msg_length;
+    buffer->msg_count++;
+}
+
+/*
+ *  Get log msg from msg buffer. Increment removal position and decrement count.
+ 
+ *  Message isn't removed in the sense that it's overwritten. Circular buffer records
+    position to get next data from, and that position gets incremented.
+ */
+char *mx_circbuf_offermsg(mx_circbuf_t *buffer)
+{
+    char *output = buffer->msg_arr[buffer->msg_first++];
+    buffer->msg_first = buffer->msg_first % buffer->msg_length;
+    buffer->msg_count--;
     return output;
 }
 
@@ -114,7 +153,7 @@ void mx_circbuf_destructor(mx_circbufint_t *DOA)
 Function headers for Networked Spell Checker.
 ************************************************/
 /*
- * Safely insert a socket descriptor into a buffers struct
+ * Safely insert a socket descriptor into a buffers-struct
     concurrently with other threads.
 
  *  Inputs
@@ -123,14 +162,36 @@ Function headers for Networked Spell Checker.
  void append_sockets(int sd);
 
 /*
- *  Safely remove socket descriptor from buffers struct concurrently
-    with other processes.
+ *  Safely remove socket descriptor from buffers-struct concurrently
+    with other threads.
  */
 int take_socket();
 
 /*
+ *  Safely insert string into buffers-struct log buffer concurrently with other
+    threads. Copies argument into heap allocated string.
+ *  Inputs
+    lookup - string containing word and response
+ */
+void append_lookups(char *lookup);
+
+/*
+ *  Safely remove string from buffers-struct log buffer concurrently with other
+    threads. 
+ */
+char *take_lookup();
+
+/*
  *  Try to consume from socket_requests
- *  Check all input received over socket.
+ *  For all connections taken from sockets buffer:
+ *      Lookup word
+ *      Send word with response appended
+ *      Log word and response
  *  When client disconnects, startover.
  */
 void *worker_routine(void *);
+
+/*
+ *  Get next lookup record from buffers-struct log buffer and print it to log file.
+ */
+void *logger_routine(void *);
